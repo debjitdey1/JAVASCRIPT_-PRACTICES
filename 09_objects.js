38070c33e894b6e00272c73c98b8bc80c5586012
Debjit Dey <121121842+debjitdey1@users.noreply.github.com>
2025-12-02 23:02:44 +0530

    Add files via upload

--- 09_objects.js
+++ 09_objects.js
@@ -11,11 +11,34 @@ let myObj = {
   India`,
   phoneNo: 8617841049,
 };
-console.log(myObj);
-// console.log(typeof myObj);
-console.log(myObj.Email); // that is not a good practice to write key name starting with capital letter
-console.log(myObj["phoneNo"]);
-console.log(myObj["adress"]);
-console.log(myObj[symp1]);
-console.log(typeof myObj[symp1]);
+// console.log(myObj);
+// // console.log(typeof myObj);
+// console.log(myObj.Email); // that is not a good practice to write key name starting with capital letter
+// console.log(myObj["phoneNo"]);
+// console.log(myObj["adress"]);
+// console.log(myObj[symp1]);
+// console.log(typeof myObj[symp1]);
 // adding new key-value pair to object
+const description = {};
+// description.ipAddress = 323453332;
+// description["macAddress"] = "00:1B:44:11:3A:B7";
+// console.log(description["macAddress"]);
+
+const object1 = { 1: "a", 2: "b" };
+const object2 = { 3: "a", 4: "b" };
+const object3 = { 5: "a", 6: "b" };
+// const mergedObject = Object.assign({}, object1, object2, object3);
+const mergedObject = { ...object1, ...object2, ...object3 };
+// console.log(mergedObject);
+
+let obb = [
+  { name: "Debjit", age: 24, hobbies: "coding" },
+  { name: "Ankit", age: 25, hobbies: "reading" },
+  { name: "Raju", age: 23, hobbies: "gaming" },
+];
+// console.log(obb);
+// console.log(object.keys(obb));
+
+console.log(Object.keys(myObj));
+console.log(Object.values(myObj));
+console.log(Object.entries(myObj));
