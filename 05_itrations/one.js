4ca809a73a6c16a0a7e669e104b0faddc90c70bf
Debjit Dey <121121842+debjitdey1@users.noreply.github.com>
2026-03-11 00:32:11 +0530

    Add files via upload

---
+++ 05_itrations/one.js
@@ -0,0 +1,20 @@
+//for
+// for (let i = 0; i <= 50; i++) {
+//   const element = i;
+//   console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   const element = i;
+//   if (element == 5) {
+//     console.log("Found 5!");
+//   }
+//   console.log(element);
+// }
+
+let myarr = ["Flash", "Arrow", "Batman"];
+console.log(myarr.length);
+for (let i = 0; i < myarr.length; i++) {
+  const element = myarr[i];
+  console.log(element);
+}
